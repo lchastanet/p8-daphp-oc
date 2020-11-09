03629b25d6ccf900da17efb577b83dbac1fb7f3e
loris <loris.chastanet@gmail.com>
2020-11-09 17:32:50 +0100

    upgrade symfony to latest lts, this close #1

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndexAction()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        //$this->assertContains('Bienvenue sur Todo List', $crawler->filter('h1'));
+    }
+}
