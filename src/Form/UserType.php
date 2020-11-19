c0f25bea214286da5fc3b49d62cc6e03ad36437b
loris <loris.chastanet@gmail.com>
2020-11-19 22:57:48 +0100

    finalize role management system, this fix #4

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,10 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
@@ -22,6 +25,30 @@ class UserType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email']);
+            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'choices' => [
+                    'Utilisateur' => "ROLE_USER",
+                    'Administrateur' => "ROLE_ADMIN",
+                ],
+                'label' => "Rôle"
+            ]);
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($tagsAsArray) {
+                    // transform the array to a string
+                    if ($tagsAsArray !== null) {
+                        return $tagsAsArray[0];
+                    }
+
+                    return null;
+                },
+                function ($tagsAsString) {
+                    // transform the string back to an array
+                    return array($tagsAsString);
+                }
+            ));
     }
 }
